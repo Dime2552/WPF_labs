14d795e557473eb4887181d3cc25be0b0c3b0cfc
Dime <163345642+Dime2552@users.noreply.github.com>
2025-03-05 15:55:11 +0200

    Update Program.cs

--- Lab1/Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Lab1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/// <summary>
+/// Основний клас програми.
+/// </summary>
+class HelloWorld
+{
+    /// <summary>
+    /// Точка входу в програму.
+    /// </summary>
+    /// <param name="args">Аргументи командного рядка.</param>
+    static void Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+    }
+}
